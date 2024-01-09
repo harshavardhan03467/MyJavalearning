3b56cb034430974425755ade6c117abc4afaec6a
hvku <hvku123@outlook.com>
2024-01-08 19:32:11 -0600

    Adding Project to the feature branch

---
+++ src/test/java/Sample.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Sample {
+	@Test
+	public void TestEquality(){
+		assertEquals(123,123);
+		
+	}
+	@Test
+	public void TestEquality1(){
+		assertEquals(123,123);
+		
+	
+	}
+	
+	
+}
